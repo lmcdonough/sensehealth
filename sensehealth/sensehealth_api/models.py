14ffe6bcd35306942a2c095e74a3d0312627819b
Levi McDonough <levimcdonough@gmail.com>
2015-12-18 16:47:17 -0500

    removed comments.

--- sensehealth/sensehealth_api/models.py
+++ sensehealth/sensehealth_api/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
 	name = models.CharField(max_length=200, blank=False, null=False)
-	# created_by = models.ForeignKey(User, related_name='category')
 
 	class Meta:
 		ordering = ('name',)
